60ad7fdff30c2bb3e2ac43ebe6b1880fa540fd40
DuncanZim <duncanfmoyo@gmail.com>
2019-07-23 14:41:58 +0200

    Note Update View

--- notepad/urls.py
+++ notepad/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from .views import create_view, list_view, delete_view
+from .views import create_view, list_view, delete_view, update_view
 
 app_name = "notepad"
 
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^create/', create_view, name='create'),
     url(r'^list/', list_view, name='list'),
     url(r'^(?P<id>\d+)/delete/', delete_view, name='delete'),
+    url(r'^(?P<id>\d+)/update/', update_view, name='update'),
 ]
