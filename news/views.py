3fd18381e63dc5bcc88c9d7436fec8e0b62a9d38
DuncanZim <duncanfmoyo@gmail.com>
2019-07-24 11:41:01 +0200

    fixed scraper in views

--- news/views.py
+++ news/views.py
@@ -1,20 +1,11 @@
-from bs4 import BeautifulSoup
-from django.shortcuts import redirect, render
+from django.shortcuts import render
 import requests
-from .models import Headline, UserProfile
-from datetime import timezone, datetime
-import os
-import shutil
+from bs4 import BeautifulSoup
 
 requests.packages.urllib3.disable_warnings()
 
 
-def scrape(request):
-    user_p = UserProfile.objects.filter(user=request.user).first()
-    if user_p is not None:
-        user_p.last_scrape = datetime.now(timezone.utc)
-        user_p.save()
-
+def scrape():
     session = requests.Session()
     session.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 '
@@ -28,34 +19,12 @@ def scrape(request):
     posts = soup.find_all('article', {'class': 'product_pod'})  # return a list
 
     for i in posts:
-        link = i.find('a', {'class': False, 'title': True})['href']
+        link = i.find('a', {'class': False})['href']
         title = i.find('img', {'class': 'thumbnail'})['alt']
         image_source = i.find('img', {'class': 'thumbnail'})['src']
-
         print(link)
         print(title)
         print(image_source)
 
-        # stackoverflow solution
 
-        media_root = 'C:/Users/Interbiz/PycharmProjects/media_root'
-        if not image_source.startswith(("data:image", "javascript")):
-            local_filename = image_source.split('/')[-1].split("?")[0]
-            r = session.get(image_source, stream=True, verify=False)
-            with open(local_filename, 'wb') as f:
-                for chunk in r.iter_content(chunk_size=1024):
-                    f.write(chunk)
-
-            current_image_absolute_path = os.path.abspath(local_filename)
-            shutil.move(current_image_absolute_path, media_root)
-
-        # end of stackoverflow solution
-
-        new_headline = Headline()
-        new_headline.title = title
-        new_headline.url = link
-        new_headline.image = local_filename
-        new_headline.save()
-
-    return redirect('/')
-
+scrape()
