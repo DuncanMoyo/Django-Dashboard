32e45ce25508dc6dcda46a80bca31300405af00d
DuncanZim <duncanfmoyo@gmail.com>
2019-07-24 23:45:05 +0200

    Dash Intro

--- finance/views.py
+++ finance/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Company
@@ -27,5 +27,25 @@ class ChartData(APIView):
         }
 
         return Response(data)
+
+### dash ###
+
+def dash(request, **kwargs):
+    return HttpResponse(dispatcher(request ))
+
+
+def dash_ajax(request):
+    return HttpResponse(dispatcher(request), content_type='application/json')
+
+
+
+
+
+
+
+
+
+
+
 
 
