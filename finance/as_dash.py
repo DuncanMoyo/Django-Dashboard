fad1c7e6a478dcca9460fe5099bb753d0e418c06
DuncanZim <duncanfmoyo@gmail.com>
2019-07-25 01:58:14 +0200

    Dash App Part 1

--- finance/as_dash.py
+++ finance/as_dash.py
@@ -27,4 +27,53 @@ def dispatcher(request):
 
 
 def _create_app():
+    app = dash.Dash(csrf_protect=False)
+    app.layout = html.Div([
+        html.Div([
+            html.H2('Quandl Finance Explorer',
+                    style={
+                        'display': 'inline',
+                        'float': 'left',
+                        'font-size': '2.65em',
+                        'margin-left': '7px',
+                        'font-weight': 'bolder',
+                        'font-family': 'Product Sans',
+                        'color': "rgba(117, 117, 117, 0.95)",
+                        'margin-top': '20px',
+                        'margin-bottom': '0'
+                    }),
+            html.A('Home', href='/home/', style={
+                'color': 'red',
+                'display': 'inline',
+                'margin-left': '54%'
+            })
+        ]),
+        dcc.Dropdown(
+            id='stock-ticker-input',
+            options=[{'label': s[0], 'value': s[1]} for s in zip(df_symbol.Company, df_symbol.Symbol)],
+            value=['AAPL', 'TSLA'],
+            multi=True
+        ),
+        html.Div(id='graphs')
+
+        @ app.callback(
+            dash.dependencies.Output('graphs', 'children'),
+            [dash.dependencies.Input('stock-ticker-input', 'value')]
+        )
+
+    )
+    ])
+    ])
+
     return
+
+
+
+
+
+
+
+
+
+
+
